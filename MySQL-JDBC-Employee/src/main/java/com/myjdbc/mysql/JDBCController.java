d54c75937ff0b8a03545de8b5c39df1aa3e02a38
john <john@john-laptop>
2017-03-06 00:59:12 +0530

    scanner object created

--- MySQL-JDBC-Employee/src/main/java/com/myjdbc/mysql/JDBCController.java
+++ MySQL-JDBC-Employee/src/main/java/com/myjdbc/mysql/JDBCController.java
@@ -3,18 +3,26 @@ package com.myjdbc.mysql;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class JDBCController {
 
 	Connection connection;
+	Scanner scanner;
 
 	public JDBCController() {
 		try {
 			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdatabase", "root", "root");
-			
-			if(connection != null){
+
+			if (connection != null) {
 				System.out.println("DATABASE CONNECTED");
+			} else {
+				System.exit(0);
 			}
+			
+			scanner = new Scanner(System.in);
+			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
